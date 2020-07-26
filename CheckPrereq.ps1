c08aa7db9979fe401be7c7b8fc81ba6cadf5dda6
BrunoRomer <40766143+BrunoRomer@users.noreply.github.com>
2020-07-25 22:35:53 -0500

    Add files via upload

---
+++ CheckPrereq.ps1
@@ -0,0 +1,8 @@
+﻿$Server = "VS01","VS02"#,"VS03","VS04"
+
+Invoke-Command -ComputerName $Server -ScriptBlock {
+#Get-WindowsFeature -Name Web-Mgmt-Service
+#Get-Item HKLM:\SOFTWARE\Microsoft\WebManagement\Server
+#Get-Service WMSVC
+shutdown -s -t 0
+}
---
+++ InstallRequ.ps1
@@ -0,0 +1,9 @@
+﻿$Server = "VS01","VS02","VS03","VS04"
+
+Invoke-Command -ComputerName $Server -ScriptBlock {
+cd HKLM:\SOFTWARE\Microsoft\WebManagement\Server
+
+Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\WebManagement\Server -Name EnableRemoteManagement -Value 1
+
+Set-Service -Name WMSVC -StartupType Automatic
+}
