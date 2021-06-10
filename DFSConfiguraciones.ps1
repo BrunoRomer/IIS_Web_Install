f146522955c7d67a4f3deae938b2678d857c914f
BrunoRomer <40766143+BrunoRomer@users.noreply.github.com>
2021-06-09 23:11:53 -0500

    Add files via upload

---
+++ DFSConfiguraciones.ps1
@@ -0,0 +1,3 @@
+﻿.\New-DfsrConfiguration.ps1 -GroupName GroupName -FolderName TuFolder -ComputerName VS01,VS02,Vs03 -PrimaryComputerName VS01 -ContentPath C:\TuFolder -StagingPathQuotaInMB (1024 * 4) -Verbose 
+
+#-ComputerName VS01,VS02,Vs03 (inicia con el servidor que sería el primario)
